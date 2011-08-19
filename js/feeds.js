511d0d01b2cb0b18024a69f7d89ba3bac24c8e89
Sean <magicalhobo@hotmail.com>
2011-08-19 00:11:57 -0700

    Using twitter feed instead.

--- js/feeds.js
+++ js/feeds.js
@@ -3,7 +3,8 @@ function commitHandler(result)
 	if(result && result.commits)
 	{
 		var divs = [];
-		for(var iter = 0; iter < 5; iter++)
+		var count = Math.min(result.commits.length, 5);
+		for(var iter = 0; iter < count; iter++)
 		{
 			var data = result.commits[iter];
 			var date = new Date(data.committed_date);
@@ -14,5 +15,23 @@ function commitHandler(result)
 			divs.push(div);
 		}
 		$('#commit-log').html(divs.join(''));
+	}
+}
+
+function tweetHandler(result)
+{
+	if(result)
+	{
+		var divs = [];
+		var count = Math.min(result.length, 10);
+		for(var iter = 0; iter < count; iter++)
+		{
+			var data = result[iter];
+			var div = '<div class="tweet">' +
+				'<div class="message">'+data.text+'</div>' +
+				'</div>';
+			divs.push(div);
+		}
+		$('#tweet-log').html(divs.join(''));
 	}
 }
